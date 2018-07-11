f87279255e8c8a2388dbabfeb866f7950ba869cf
jinkelazzz <330751734@qq.com>
2018-07-11 09:26:03 +0800

    新增双障碍二元期权

---
+++ src/option/DoubleBinaryBarrierOption.java
@@ -0,0 +1,16 @@
+package option;
+
+/**
+ * @author liangcy
+ */
+public class DoubleBinaryBarrierOption extends BaseSingleOption{
+    @Override
+    public double bsm() {
+        return 1.2;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
